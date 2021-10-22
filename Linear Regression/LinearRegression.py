28accbab61c6526d0bad81833ccf3333f4d295d9
ShiyangLi-Shirley <59629390+ShiyangLi-Shirley@users.noreply.github.com>
2021-10-22 17:37:47 -0600

    Finish stochastic algorithm

--- Linear Regression/LinearRegression.py
+++ Linear Regression/LinearRegression.py
@@ -1,8 +1,15 @@
+"""
+@author: Shirley (Shiyang) Li
+Linear Regression by using batch gradient descent algorithm and stochastic gradient descent algorithm
+"""
 import numpy as np
 from numpy import linalg as lin
 import Read
 
-algorithm = input()
+algorithm_type = input("Enter the algorithm you'd like to use(bgd for batch gradient descent or sgd for stochastic "
+                       "gradient descent):")
+
+
 # batch gradient descent algorithm
 def compute_gradient(weights):
     gradient = float(0)
@@ -16,9 +23,9 @@ def compute_cost(data, y, weights):
     sum_squared_error = float(0)
     num_examples = len(data)
     for i in range(num_examples):
-        squared_error = np.square(y[i] - data[i] * np.transpose(weights))#data??????
+        squared_error = np.square(y[i] - data[i] * np.transpose(weights))  # data??????
         sum_squared_error += squared_error
-    return float(1/2 * sum_squared_error)
+    return float(1 / 2 * sum_squared_error)
 
 
 def batch_gradient_descent():
@@ -30,6 +37,7 @@ def batch_gradient_descent():
         weights = np.zeros(7)
         weights = np.asmatrix(weights)
         cost_func_val = ""
+
         for t in range(73):
             # compute gradient
             gradient = compute_gradient(weights)
@@ -47,10 +55,39 @@ def batch_gradient_descent():
 
 # stochastic gradient descent algorithm
 def stochastic_gradient_descent():
-    weights_pre = np.zeros(7)
-    weights_pre = np.asmatrix(weights_pre)
-    weights = np.zeros(7)
-    weights = np.asmatrix(weights)
+    learning_rate = 1
+
+    while True:
+        weights_pre = np.zeros(7)
+        weights_pre = np.asmatrix(weights_pre)
+        weights = np.zeros(7)
+        weights = np.asmatrix(weights)
+        cost_func_val = ""
+        cost_pre = float('inf')
+        cost = float(0)
+
+        for t in range(73):
+            # randomly sample a training example
+            np.random.shuffle(train_full_data)
+            random_data = train_full_data[:, :train_data.size // len(train_data)].reshape(train_data.shape)
+            random_y = train_full_data[:, :y_train.size // len(y_train)].reshape(y_train.shape)
+
+            for i in range(len(train_data)):
+                cost_pre = cost
+                weights_pre = weights
+                for j in range(7):
+                    hyp = random_data[i] * np.transpose(weights_pre)
+                    error = random_y[i] - hyp
+                    weights = weights_pre + learning_rate * error * random_data[i, j]
+
+                cost = compute_cost(random_data, random_y, weights)
+                cost_func_val += str(cost) + ","
+
+            if np.abs(cost - cost_pre) < 10E-6:
+                return weights, learning_rate
+
+        print(cost_func_val)
+        learning_rate *= 0.5
 
 
 if __name__ == '__main__':
@@ -58,13 +95,23 @@ if __name__ == '__main__':
     train_data = np.asmatrix(train_data)
     y_train = Read.read_data("concrete/train.csv", 53)[1]
     y_train = np.asmatrix(y_train)
+    train_full_data = Read.read_data("concrete/train.csv", 53)[2]
+    train_full_data = np.asmatrix(train_full_data)
 
     test_data = Read.read_data("concrete/test.csv", 50)[0]
     test_data = np.asmatrix(test_data)
     y_test = Read.read_data("concrete/test.csv", 50)[1]
     y_test = np.asmatrix(y_test)
+    test_full_data = Read.read_data("concrete/test.csv", 50)[2]
+    test_full_data = np.asmatrix(test_full_data)
 
-    batch_result = batch_gradient_descent()
-    print("Weight: ", batch_result[0])
-    print("Learning Rate: ", batch_result[1])
-    print("Test Data Cost Function Value", compute_cost(test_data, y_test, batch_result[0]))
+    if algorithm_type == "bgd":
+        batch_result = batch_gradient_descent()
+        print("Weight: ", batch_result[0])
+        print("Learning Rate: ", batch_result[1])
+        print("Test Data Cost Function Value", compute_cost(test_data, y_test, batch_result[0]))
+    elif algorithm_type == "sgd":
+        stochastic_result = stochastic_gradient_descent()
+        print("Weight: ", stochastic_result[0])
+        print("Learning Rate: ", stochastic_result[1])
+        print("Test Data Cost Function Value", compute_cost(test_data, y_test, stochastic_result[0]))
--- Linear Regression/Read.py
+++ Linear Regression/Read.py
@@ -1,16 +1,23 @@
+"""
+@author: Shirley (Shiyang) Li
+Read data from a csv file path as two parts, the first is the
+attributes values and the second is the label values
+"""
 import numpy as np
 
 
 def read_data(path, num_examples):
+    full_data = np.zeros((num_examples, 8))
     data = np.zeros((num_examples, 7))
-    y = np.zeros((num_examples, 1))
+    y = np.zeros((num_examples, 1))  # for matrix format use
 
     with open(path, 'r') as f:
         i = 0
         for line in f:
             terms = line.strip().split(',')
+            full_data[i] = terms
             data[i] = terms[0: 7]
             y[i] = terms[7]
             i += 1
 
-    return data, y
+    return data, y, full_data
